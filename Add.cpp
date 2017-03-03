017d6d36bf0d56311fabcd30b7f12c9c31de7298
blitzbutter <blitzippy@gmail.com>
2017-03-03 16:19:05 -0500

    Create Add.cpp

---
+++ Add.cpp
@@ -0,0 +1,15 @@
+// Add two numbers 
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int num1,num2;
+    scanf("%d %d",&num1,&num2);
+    int sum; 
+    sum = solveMeFirst(num1,num2);
+    printf("%d",sum);
+    return 0;
+}
+
